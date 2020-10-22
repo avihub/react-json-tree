25e56c4969e51a61371cf79d99de7d6566b2d784
OFFICE\avi.y <avi.y@taboola.com>
2020-10-22 23:54:04 +0300

    removed unused comments

--- src/components/UrlField.js
+++ src/components/UrlField.js
@@ -1,39 +1,6 @@
 import React from 'react';
 
 function UrlField({value, onInput}) {
-
-    // const init = () => {
-    //     let waitingReq = null;
-    //     document.getElementById("url").addEventListener('input', prapareReq);
-    //     const jsonContainer = document.getElementById("json-view");
-
-    //     function prapareReq(e) {
-    //         if (waitingReq) {
-    //             window.clearTimeout(waitingReq);
-    //         }
-    //         waitingReq = setTimeout(makeRequest.bind(this, e), 1000);
-    //     }
-
-    //     function makeRequest(e) {
-    //         fetch(e.target.value)
-    //             .then(response => {
-    //                 if (!response.ok) {
-    //                     throw new Error('response status code is not ok');
-    //                 }
-    //                 return response.json()
-    //             }
-    //             )
-    //             .then(json => {
-    //                 jsonContainer.appendChild(createJsonTree(json));
-    //             }
-    //             )
-    //             .catch(error => {
-    //                 alert(`error: ${error}`);
-    //             });
-    //     }
-    // }
-    // init();
-
     return (
         <div className='url-field'>
             <h1>
