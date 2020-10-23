d5c5e4497096e9409c16ae2073fdb1ad15d8ab3d
OFFICE\avi.y <avi.y@taboola.com>
2020-10-24 01:33:32 +0300

    optimization - rendering the jsonTree only once we get a valid JSON response from URL

--- src/components/App.js
+++ src/components/App.js
@@ -3,47 +3,17 @@ import './App.css';
 import UrlField from './UrlField';
 import JsonTree from './jsonTree/JsonTree';
 
-let waitingReq = null;
-
 function App() {
-    const [value, setValue] = useState('');
     const [json, setJson] = useState('');
 
 
-    function handleOnInput(e) {
-        setValue(e.target.value);
-
-        prapareReq(e);
-    }
-
-    function prapareReq(e) {
-        if (waitingReq) {
-            window.clearTimeout(waitingReq);
-        }
-        waitingReq = setTimeout(makeRequest.bind(this, e), 1000);
-    }
-    
-    function makeRequest(e) {
-        fetch(e.target.value)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('response status code is not ok');
-                }
-                return response.json()
-            }
-            )
-            .then(json => {
-                setJson(json);
-            }
-            )
-            .catch(error => {
-                alert(`error: ${error}`);
-            });
+    function updateJsonTree(json) {
+        setJson(json);
     }
 
     return (
         <div className="App">
-            <UrlField value={value} onInput={handleOnInput} />
+            <UrlField updateJsonTree={updateJsonTree} />
             <JsonTree jsonNode={json} />
         </div>
     );
--- src/components/UrlField.js
+++ src/components/UrlField.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function UrlField({value, onInput}) {
+let waitingReq = null;
+
+function UrlField({ updateJsonTree }) {
+    const [url, setUrl] = useState('');
+
+import React from 'react';
+    function onInput(e) {
+        const url = e.target.value;
+        setUrl(url);
+        prapareReq(url);
+    }
+
+    function prapareReq(url) {
+        if (waitingReq) {
+            window.clearTimeout(waitingReq);
+        }
+        waitingReq = setTimeout(makeRequest.bind(this, url), 1000);
+    }
+
+    function makeRequest(url) {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('response status code is not ok');
+                }
+                return response.json()
+            }
+            )
+            .then(json => {
+                updateJsonTree(json);
+            }
+            )
+            .catch(error => {
+                console.log(`error: ${error}`);
+            });
+    }
+
     return (
         <div className='url-field'>
             <h1>
                 Json viewer
             </h1>
-            <input type="url" name="url" id="url" placeholder="https://example.com" 
-                value={value}
+            <input type="url" name="url" id="url" placeholder="https://example.com"
+                url={url}
                 onInput={onInput}
             />
             <div id="json-view"></div>
